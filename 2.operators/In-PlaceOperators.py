030a4e79ec0230ba34201317b1624c3bdb909658
Bikram <bikram.keshari.das@gmail.com>
2018-04-18 00:46:42 +0530

    additional info added

--- 2.operators/In-PlaceOperators.py
+++ 2.operators/In-PlaceOperators.py
@@ -1,4 +1,5 @@
 #In-place operators allow you to write code like 'x = x + 3' more concisely, as 'x += 3'. 
+#The same thing is possible with other operators such as -, *, / and % as well.
 x = 2
 print (x)
 
