f3745713936a37bdda0a1ef140109388d36b5268
Bikram <bikram.keshari.das@gmail.com>
2018-04-18 00:03:02 +0530

    Basic variable concept

---
+++ 5.variable/basic-variable.py
@@ -0,0 +1,11 @@
+# A variable allows you to store a value by assigning it to a name,
+# which can be used to refer to the value later in the program. 
+
+var=10
+print (var)
+
+var2 = "Bikram"
+
+print (var2)
+
+print (var2 * 3)
