b0899b896248874a94064e1c2e74cf106c9fd77b
Bikram <bikram.keshari.das@gmail.com>
2018-04-18 00:33:35 +0530

    added variable naming rules

---
+++ 5.variable/basic-variable3.py
@@ -0,0 +1,13 @@
+#In Python The only characters that are allowed for varibale names are
+# letters, numbers, and underscores. Also, they can't start with numbers.
+
+this_is_valid_variable = 7
+print (this_is_valid_variable)
+
+12var = 7
+
+print (12var)  # This is an invalid variable name  #O/P:-invalid syntax
+
+var test = 5 
+
+print (var test) # Space is not allowed in variable name #O/P:- invalid syntax
