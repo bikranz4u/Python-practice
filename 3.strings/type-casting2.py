3afacd550197f89e1c6004b4b1d4fb5864968b4c
Bikram <bikram.keshari.das@gmail.com>
2018-04-17 23:32:40 +0530

    Update type casting with print statement

---
+++ 3.strings/type-casting2.py
@@ -0,0 +1,4 @@
+#Another example of type conversion is turning user input (which is a string) to numbers (integers or floats),
+# to allow for the performance of calculations.
+
+float(input("Enter a number: ")) + float(input("Enter another number: "))
