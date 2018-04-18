e9750dcd03944b1a6b329b1936f24a3a8d21b340
Bikram <bikram.keshari.das@gmail.com>
2018-04-18 17:54:30 +0530

    Added another nested example

---
+++ ControlStructure/else-if.py
@@ -0,0 +1,11 @@
+#An else statement follows an if statement, 
+#and contains code that is called when the if statement evaluates to False. 
+
+#As with if statements, the code inside the block should be indented.
+
+num = int(input ("Enter a number:"))
+if num > 10:
+    print ("Num is greater than 10")
+else:
+    print ("Num is lesser than 10")
+   
---
+++ ControlStructure/if-statement-nested2.py
@@ -0,0 +1,5 @@
+if 1 + 1 == 2:  #O/P:- True #First statement evaluates to TRUE, so 2nd statement checked and Evaluates to FALSE
+    if 2 * 2 == 8:  #O/P:- False
+        print("if") 
+    else:
+        print("else")  #This will be displayed in terminal
